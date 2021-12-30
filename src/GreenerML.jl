1f027b316aa3ccfd7a508e586d15cd9c0e6c5f88
Russell Jarvis <russelljarvis@protonmail.com>
2021-12-30 15:08:12 +1100

    for remote setup

---
+++ src/GreenerML.jl
@@ -0,0 +1,20 @@
+module GreenerML
+
+using PyCall
+using OrderedCollections
+using LinearAlgebra
+using DataStructures
+using UnicodePlots
+using Statistics
+using JLD
+using Plots
+using Distributions
+using LightGraphs
+using Metaheuristics
+using SignalAnalysis
+using SpikeNN
+#include("current_search.jl")
+#include("utils.jl")
+#include("sdo_network.jl")
+
+end
